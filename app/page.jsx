6b231261a613069b634293df4edb082372a6e688
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-27 18:30:45 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-'use client'
-import React from "react";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
 import Banner from "@/components/Banner";
@@ -8,20 +6,20 @@ import FeaturedProduct from "@/components/FeaturedProduct";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const Home = () => {
+export default function Home() {
   return (
     <>
-      <Navbar/>
-      <div className="px-6 md:px-16 lg:px-32">
+      <Navbar />
+
+      <main className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <HomeProducts />
         <FeaturedProduct />
         <Banner />
         <NewsLetter />
-      </div>
+      </main>
+
       <Footer />
     </>
   );
-};
-
-export default Home;
+}
